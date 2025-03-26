c27fa51fb8dafac4ebac224593279070f63f6d6b
thorvi <euvitoreu@gmail.com>
2025-03-26 16:30:22 -0300

    Adds:
    * 'History', 'Home' and its routes.
    
    * define global styled components patters
    
    * initialize dependencies

---
+++ src/pages/Home.tsx
@@ -0,0 +1,3 @@
+export function Home() {
+  return <h1>Holmes</h1>
+}
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,22 @@
+export const defaultTheme = {
+  white: '#FFF',
+
+  'gray100': '#E1E1E6',
+  'gray300': '#C4C4CC',
+  'gray400': '#8D8D99',
+  'gray500': '#7C7C8A',
+  'gray600': '#323238',
+  'gray700': '#29292E',
+  'gray800': '#202024',
+  'gray900': '#121214',
+
+  'green300': '#cda364',
+  'green500': '#875800',
+  'green700': '#5f4c01',
+
+  'red500': '#AB222E',
+  'red700': '#7A1921',
+
+
+  'yellow500': '#FBA94C',
+}
